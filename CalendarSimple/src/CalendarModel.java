31213f9d93af258ac35b0dfd25660e6cf54f3464
ajsheffield <andrew.sheffield@sv-code.com>
2014-04-04 11:17:29 -0700

    Creating Data model for calendar

---
+++ CalendarSimple/src/CalendarModel.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sheff
+ */
+public class CalendarModel {
+    ArrayList<Event> events = new ArrayList<>();
+    
+    public void addEvent(Event e) {
+        
+    }
+    
+    public ArrayList<Event> getData() {
+        return events;
+    }
+    
+    /*repaint view*/
+    
+}
